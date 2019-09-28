3ab53b32092eca007ee6d21b68bc99e0e80da150
Subangkar-HP-Win10 <subangkar.karmaker@gmail.com>
2019-09-28 10:33:09 +0600

    sensor interfacing separated

--- inc/rawsensordata.h
+++ inc/rawsensordata.h
@@ -2,18 +2,37 @@
 #define __rawsensordata_H__
 
 #include <dlog.h>
+#include <service_app.h>
+#include <sensor.h>
 
 #ifdef  LOG_TAG
 #undef  LOG_TAG
 #endif
 #define LOG_TAG "X_rawsensordata"
 
-//#ifndef RAW_SENSOR_DATA_H__
-//#define RAW_SENSOR_DATA_H__
+#define LAUNCHER_APP_ID "org.example.drgbtppg" // an ID of the UI application of our package
+// -------------------------- Data Type Definitions Start ----------------------------------------
+
+// application data (context) that will be passed to functions when needed
+typedef struct appdata
+{
+    sensor_h sensor; // sensor handle
+    sensor_listener_h listener; // sensor listener handle
+} appdata_s;
 
 
-#define array_size 10000
+typedef enum {
+	HEART_RATE,PPG,ACCELEROMETER_X,ACCELEROMETER_Y,ACCELEROMETER_Z,GYROSCOPE_X,GYROSCOPE_Y,GYROSCOPE_Z,PRESSURE,GRAVITY_X,GRAVITY_Y,GRAVITY_Z,ALL
+} sensor_t;
 
-//#endif /* RAW_SENSOR_DATA_H__ */
+struct sensor_values {
+	float hr, ppg, acc_x, acc_y, acc_z, gyr_x, gyr_y, gyr_z, pres, grav_x,
+			grav_y, grav_z;
+} all_sensor_current_vals;
+
+typedef enum {
+	STOPPED, RUNNING
+} service_state_t;
+// -------------------------- Data Type Definitions End ----------------------------------------
 
 #endif /* __rawsensordata_H__ */
