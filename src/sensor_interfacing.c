d05392d22588a200fd5ac4e0cccb7501cbfc73f8
Subangkar-HP-Win10 <subangkar.karmaker@gmail.com>
2020-02-26 01:48:02 +0600

    Merge remote-tracking branch 'origin/master'

--- src/sensor_interfacing.c
+++ src/sensor_interfacing.c
@@ -17,7 +17,7 @@
 #include <activity_recognition.h>
 
 // --------------------------------------- External Functions ---------------------------------------
-//int uploadAllFiles(const char* dir);
+int uploadAllFiles(const char* dir);
 // --------------------------------------------------------------------------------------------------
 
 #define array_size 10000
@@ -318,7 +318,7 @@ Eina_Bool pause_sensors(void *vc) {
 	stop_sensors();
 	close_current_data_file();
 
-//	upload_data(vc);
+	upload_data(vc);
 
 	pause_timer=NULL;
 	return ECORE_CALLBACK_CANCEL;
